466e60c1fd3873f8f9a73b03af261e733933b2c7
yangshi <yangshinembang@gmail.com>
2026-09-05 12:29:14 +0900

    Ignore instance folder

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    review_text = db.Column(db.Text, nullable=False)
+    favorite_quote = db.Column(db.Text)
+    cover_image = db.Column(db.String(300))
+    likes = db.Column(db.Integer, default=0)
+    dislikes = db.Column(db.Integer, default=0)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow)
+
+class JournalEntry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+
 @app.route('/')
 def home():
-    return render_template('index.html')
+    reviews = Review.query.order_by(Review.date_added.desc()).all()
+    return render_template('index.html', reviews=reviews)
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
